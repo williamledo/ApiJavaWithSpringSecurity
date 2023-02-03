b4e5977b00ac782587afa030f7c3e256d6df7df1
William Lêdo <williamledoc@gmail.com>
2023-02-02 22:49:22 -0300

    controller de autenticação

--- src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -1,6 +1,12 @@
 package med.voll.api.domain.usuario;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,7 +24,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario implements Serializable {
+public class Usuario implements Serializable, UserDetails {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -26,5 +32,34 @@ public class Usuario implements Serializable {
 	private Long id;
 	private String login;
 	private String senha;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+	@Override
+	public String getUsername() {
+		return login;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 	
 }
--- src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -2,22 +2,38 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-	@Bean //Expõe o retorno do método para o spring, devolve um objeto para o spring ou um objeto que eu possa injetar em algum controller, service, etc
+	@Bean //Serve para exportar uma classe para o Spring, fazendo com que el consiga carregá-la e realize a injeção de dependência em outras classes
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
 		return http.csrf().disable() //desabilitando a proteção contra cross site request forgery, o token já protege 
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) 
 				.and().build(); //desabilitando o processo de autenticação que o spring dá um formulário e a aplicação é stateful, agora é stateless
 		
+	}
+	
+	@Bean // Estou mostrando para o spring como ele injeta objetos
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		
+		return configuration.getAuthenticationManager();
+		
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
